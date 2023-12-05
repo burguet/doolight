d8a856c00aa56ffcb0de5fd01983deaaa1bd3222
burguet <lucasburguet22020@gmail.com>
2023-12-05 10:47:58 +0100

    exo31

---
+++ src/composant/LoginForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [isError, setIsError] = useState(false);
+
+  const handleLoginChange = (e) => {
+    setLogin(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    // Vérifier si le mot de passe est différent de '1234' et définir isError en conséquence
+    setIsError(e.target.value !== '1234');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Vous pouvez ajouter ici la logique de gestion de la connexion
+    // Par exemple, vous pouvez envoyer les données au backend pour vérification
+  };
+
+  return (
+    <div>
+        <h1>Exo31</h1>
+    <form onSubmit={handleSubmit} style={{ backgroundColor: isError ? 'red' : 'white' }}>
+      <label>
+        Login:
+        <input type="text" value={login} onChange={handleLoginChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+    </div>
+  );
+};
+
+export default LoginForm;
