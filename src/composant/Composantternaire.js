7d32b13a164beaf7acee92e7ebe5285b989634d6
burguet <lucasburguet22020@gmail.com>
2023-12-05 11:22:42 +0100

    exo 30 ternaire

--- src/composant/Composantternaire.js
+++ src/composant/Composantternaire.js
@@ -1,19 +1,47 @@
-// ComposantAvecTernaire.js
-import React from 'react';
+import React, { Component } from 'react';
 
-const ComposantAvecTernaire = () => {
-  const isLoggedIn = true; // Remplacez par votre logique de connexion
+class ComposantAvecInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputContent: '',
+      isBothFilled: false,
+    };
+  }
 
-  return (
-    <div>
-      <h1>EXO30 ternaire</h1>
-      {isLoggedIn ? (
-        <p>Vous êtes connecté !</p>
-      ) : (
-        <p>Veuillez vous connecter pour accéder au contenu.</p>
-      )}
-    </div>
-  );
-};
+  handleInputChange = (e) => {
+    const newInputContent = e.target.value;
+    const newIsBothFilled = newInputContent.trim() !== '';
+    
+    this.setState({
+      inputContent: newInputContent,
+      isBothFilled: newIsBothFilled,
+    });
+  };
 
-export default ComposantAvecTernaire;
+  render() {
+    const { isBothFilled, inputContent } = this.state;
+
+    return (
+      <div>
+        <h2>Exo30 ternaire</h2>
+        <input
+          type="text"
+          value={inputContent}
+          onChange={this.handleInputChange}
+          placeholder="Saisissez quelque chose"
+        />
+
+        
+        {isBothFilled ? (
+          <p style={{ color: 'green' }}>Le champ est renseigné!</p>
+        ) : (
+          <p>Remplissez le champ pour voir le changement de couleur.</p>
+        )}
+      </div>
+    );
+  }
+}
+
+export default ComposantAvecInput;
+
